00d5dd17a95497aa2885190f7565beac82f89e00
SamBS1 <beksultanov12@gmail.com>
2023-03-07 12:08:57 +0600

    added parallax on homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import '../styles/HomePage.css'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import img1 from '../pics/img1.jpg'
+
+
 
 const HomePage = () => {
+
   return (
-    <div>HomePage</div>
+    <Parallax pages={2}>
+      <ParallaxLayer offset={0} speed={1} factor={1} style={{
+        backgroundImage: `url(${img1})`,
+        backgroundSize: 'cover'
+      }}>
+      </ParallaxLayer>
+
+      {/* <ParallaxLayer offset={1} speed={0.5} factor={1} style={{
+        backgroundImage: `url(${BGSecond})`,
+        backgroundSize: 'cover'
+      }}>
+        
+      </ParallaxLayer> */}
+    </Parallax>
   )
+  
+
 }
 
 export default HomePage
